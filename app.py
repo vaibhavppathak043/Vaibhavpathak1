9e67e629e75bed041e392b78f386bdc90046b727
vaibhavppathak043 <vaibhavppathak043@gmail.com>
2026-07-04 15:23:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ st.dataframe(df)
 X = df[['age']]
 y = df['bought_insurance']
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, train_size=0.8, random_state=42
+    X, y, train_size=0.8, random_state=42)
 
 model = LogisticRegression()
 model.fit(X_train, y_test)
